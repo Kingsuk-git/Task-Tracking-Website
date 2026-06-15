070c27e67f5c8d2df8aecaec74c1dfd6ec652655
Kingsuk Datta <kingsukdutta55@gmail.com>
2026-06-16 02:49:26 +0530

    Additional Functionality

--- src/main/java/com/task/taskTracker/controllers/TaskController.java
+++ src/main/java/com/task/taskTracker/controllers/TaskController.java
@@ -1,12 +1,10 @@
 package com.task.taskTracker.controllers;
 
 import com.task.taskTracker.domain.dto.TaskDto;
+import com.task.taskTracker.domain.entities.Task;
 import com.task.taskTracker.mappers.TaskMapper;
 import com.task.taskTracker.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -29,5 +27,15 @@ public class TaskController {
                 .stream()
                 .map(taskMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public  TaskDto createTask(@PathVariable("task_list_id") UUID taskListId
+    ,@RequestBody TaskDto taskDto){
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+        return taskMapper.toDto(createdTask);
     }
 }
